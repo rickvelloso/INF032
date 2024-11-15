7522637d97ec8d563f035a11d7706b5bf2e52cdc
Pedro Henrique Barreto Velloso <pedrohenriquevelloso18@gmail.com>
2024-11-15 00:01:35 -0300

    Ex 001 da lista 001 do livro

---
+++ Lista001Livro/ex001.py
@@ -0,0 +1,25 @@
+import math
+
+# (a) y = 4^3 - 2^2
+y = 4**3 - 2**2
+print("y =", y)
+
+# (b) x = sin(2) - cos(4.2)
+x = math.sin(2) - math.cos(4.2)
+print("x =", x)
+
+# (c) z = cos(sin(3.7) - tan(1.3))
+z = math.cos(math.sin(3.7) - math.tan(1.3))
+print("z =", z)
+
+# (d) Resto da divisão de 26 por 4
+resto = 26 % 4
+print("Resto da divisão de 26 por 4:", resto)
+
+# (e) Converter x = 46.2° para radianos
+x_radianos = math.radians(46.2)
+print("x em radianos:", x_radianos)
+
+# (f) Converter y = 3.1 rad para graus
+y_graus = math.degrees(3.1)
+print("y em graus:", y_graus)
